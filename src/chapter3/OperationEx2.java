785a8d15aff097ff2a55b3bb10b49418e71266a6
jeong-inn <jung1nn@naver.com>
2022-02-10 16:52:46 +0900

    Chapter3

---
+++ src/chapter3/OperationEx2.java
@@ -0,0 +1,21 @@
+package chapter3;
+
+public class OperationEx2 {
+
+	public static void main(String[] args) {
+		int gameScore = 150;
+		
+		int lastScore1 = ++gameScore;
+		System.out.println(lastScore1);
+		
+		int lastScore2 = --gameScore;
+		System.out.println(lastScore2);
+		
+		int lastScore3 = gameScore++;
+		System.out.println(lastScore1);
+		
+		int lastScore4 = gameScore--;
+		System.out.println(lastScore2);
+	}
+
+}
