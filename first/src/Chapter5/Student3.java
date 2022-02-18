45b99ad7f0a10c7c9687e21c99e1ca1890df9921
jeong-inn <jung1nn@naver.com>
2022-02-18 17:50:36 +0900

    no message

--- first/src/Chapter5/Student3.java
+++ first/src/Chapter5/Student3.java
@@ -3,6 +3,6 @@ package Chapter5;
 public class Student3 {
 	int studentID;
 	String studentName;
-	Subject Korean;
-	Subject math;
+	Sbuject Korean;
+	Sbuject math;
 }
---
+++ first/src/module-info.java
@@ -0,0 +1,13 @@
+module Java_Sturdy {
+	exports Chapter7.StudentClass;
+	exports chapter4;
+	exports Chapter6;
+	exports Chapter5;
+	exports chapter3;
+	exports chapter2;
+	exports Chapter8;
+	exports Chapter7;
+	exports Chapter8.polymorphism;
+	exports Chapter8.CustomerInheritance;
+	exports Chapter6.cooperation;
+}
